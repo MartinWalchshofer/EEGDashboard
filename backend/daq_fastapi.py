da0521a9b9de779e074a59d95b6a368287380ea2
Martin Walchshofer <mwalchshofer@gmx.net>
2024-02-02 20:04:22 +0100

    updated  fastapi example

--- backend/daq_fastapi.py
+++ backend/daq_fastapi.py
@@ -1,14 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from daq_simulator import DAQSimulator
+import json
+
+apiStartScanning = 'StartScanning'
+apiStopScanning = 'StopScanning'
+apiGetAvailableDevices = 'GetAvailableDevices'
+apiOpen = 'Open'
+apiClose = 'Close'
 
 app = FastAPI()
+
+discovered_devices = []
+def on_devices_discovered(devices):
+    global discovered_devices
+    discovered_devices = devices
 
 #start with 'uvicorn daq_fastapi:app --port 5832'
 @app.post("/api/")
 async def create_item(item_data: dict):
     command = item_data.get('cmd')
     res = []
-    if command == 'StartScanning':
+    if command == apiStartScanning:
+        DAQSimulator.add_devices_discovered_eventhandler(on_devices_discovered)
+        DAQSimulator.start_scanning()
         res = {"msg": 'Scanning for devices...'}
-    elif command == 'StartScanning':
+    elif command == apiStopScanning:
+        DAQSimulator.remove_devices_discovered_eventhandler(on_devices_discovered)
+        DAQSimulator.stop_scanning()
         res = {"msg": 'Stopped scanning for devices...'}
-    return {"msg": 'Scanning for devices...'}
+    elif command == apiGetAvailableDevices:
+        res = {'devices': discovered_devices}
+    else:
+        raise HTTPException(status_code=400, detail="Unknown api command")
+    return res
--- bci-dashboard/renderer.js
+++ bci-dashboard/renderer.js
@@ -1,16 +1,57 @@
-document.getElementById('sendButton').addEventListener('click', async () => {
-    console.log('test');
+//API functions
+const apiPath = 'http://localhost:5832/api/' // TODO PUT INTO .env
+const apiStartScanning = 'StartScanning';
+const apiStopScanning = 'StopScanning';
+const apiGetAvailableDevices = 'GetAvailableDevices';
+const apiOpen = 'Open'
+const apiClose = 'Close'
+
+//variables
+var isScanning = false;
+
+//UI elements
+const btnStartStop = document.getElementById('btnStartStop');
+
+//scan for available devices
+const scanningTask = setInterval(GetAvailableDevices, 500);
+
+//start scanning for devices
+try{
+    StartScanning();
+}
+catch (error) {
+    console.error('Message', 'Ensure that the backend server is running.')
+    console.error('Error:', error);
+}
+
+btnStartStop.addEventListener('click', () => {
+    StartScanning();
+});
+
+async function GetAvailableDevices() {
+    await SendAPIRequest(apiGetAvailableDevices);
+}
+
+async function StartScanning() {
+    await SendAPIRequest(apiStartScanning);
+}
+
+async function StopScanning() {
+    await SendAPIRequest(apiStopScanning);
+}
+
+async function SendAPIRequest(command) {
     try {
-        const response = await fetch('http://localhost:5832/api/', {
+        const response = await fetch(apiPath, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ cmd: 'StartScanning' })
+            body: JSON.stringify({ cmd: command })
         });
         const data = await response.json();
         console.log('Response:', data);
     } catch (error) {
         console.error('Error:', error);
     }
-});
+}
