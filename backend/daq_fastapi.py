bd80629988fe894bc19556af675ee357a77ab05b
Martin Walchshofer <mwalchshofer@gmx.net>
2024-02-03 12:57:25 +0100

    Update daq_fastapi.py

--- backend/daq_fastapi.py
+++ backend/daq_fastapi.py
@@ -7,12 +7,19 @@ apiGetAvailableDevices = 'GetAvailableDevices'
 apiOpen = 'Open'
 apiClose = 'Close'
 
+samplingRate = 250
+numberOfChannels = 8
+
 app = FastAPI()
+device = None
 
 discovered_devices = []
 def on_devices_discovered(devices):
     global discovered_devices
     discovered_devices = devices
+
+def on_data_available(data):
+    print(data) #Write to buffer
 
 #start with 'uvicorn daq_fastapi:app --port 5832'
 @app.post("/api/")
@@ -29,6 +36,16 @@ async def create_item(item_data: dict):
         res = {"msg": 'Stopped scanning for devices...'}
     elif command == apiGetAvailableDevices:
         res = {'devices': discovered_devices}
+    elif command == apiOpen:
+        deviceName = item_data.get('args')
+        if deviceName not in discovered_devices:
+            raise HTTPException(status_code=400, detail="Device not found")
+        device = DAQSimulator(deviceName, samplingRate, numberOfChannels)
+        device.add_data_available_eventhandler(on_data_available)
+    elif command == apiClose:
+        device.remove_data_available_eventhandler(on_data_available)
+        del device
+        device = None
     else:
         raise HTTPException(status_code=400, detail="Unknown api command")
     return res
@@ -37,6 +54,7 @@ async def create_item(item_data: dict):
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
+        #TODO READ FROM BUFFER AND SEND
         #TODO SEND DATA HERE
         data = await websocket.receive_text() #TODO REMOVE
         await websocket.send_text(f"Message text was: {data}") #TODO REMOVE
