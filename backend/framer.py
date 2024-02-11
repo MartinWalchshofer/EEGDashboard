35484b7f188674cdd59d844be207fb10001dcc13
Martin Walchshofer <mwalchshofer@gmx.net>
2024-02-11 20:11:16 +0100

    Update framer.py

--- backend/framer.py
+++ backend/framer.py
@@ -2,11 +2,9 @@ import numpy as np
 
 class Framer:
     __frame = None
-    __frameOut = None
     __cnt = 0
     def __init__(self, samples_count, channel_count):
         self.__frame = np.zeros((samples_count, channel_count))
-        self.__frameOut = np.zeros((samples_count, channel_count))
         self.__cnt = 0
 
     def setData(self, data):
@@ -14,8 +12,12 @@ class Framer:
             data = np.array([data])
 
         if isinstance(data, np.ndarray):
-            if data.shape[1] is not self.__frame.shape[1]:
+            if data.ndim is 1:
+                data = data[np.newaxis,:]
+            if data.ndim is not 2:
                 raise ValueError("Dimensions do not match.")
+            if data.shape[1] is not self.__frame.shape[1]:
+                raise ValueError("Number of channels do not match.")
             for sample in range(0, data.shape[0]):
                 for channel in range(0, data.shape[1]):
                     self.__frame[self.__cnt, channel] = data[sample, channel]
